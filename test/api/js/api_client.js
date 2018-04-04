a115d4a68f8b3bd1653f5bcf67e19d3698d9ef79
Peter Marklund <peter@marklunds.com>
2018-04-04 22:13:37 +0200

    API test improvements - print last HTTP response on assertion failure. Print successful stats as well (number of api calls etc)

--- test/api/js/api_client.js
+++ test/api/js/api_client.js
@@ -3,14 +3,6 @@ const axios = require('axios').create({validateStatus: (status) => status < 500}
 const _assert = require('assert')
 const {uuid} = require('lib/util')
 
-function assertEqual (actual, expect) {
-  _assert.deepEqual(actual, expect)
-}
-
-function assert (actual) {
-  _assert(actual)
-}
-
 function isMongoId (id) {
   return id && id.match(/[a-z0-9]{24}/)
 }
@@ -35,14 +27,46 @@ function printExpect (expect) {
 }
 
 function client ({BASE_URL}) {
-  const requests = []
+  const self = {
+    suite: null,
+    requests: []
+  }
+
+  function printLastResult () {
+    const result = self.requests[self.requests.length - 1].result
+    console.log(`Last API response status=${result.status}`)
+    console.log(json(result.data))
+  }
+
+  function assertEqual (actual, expected) {
+    try {
+      _assert.deepEqual(actual, expected)
+    } catch (err) {
+      console.log('assertEqual failed')
+      console.log(`actual='${json(actual)}'`)
+      console.log(`expected='${json(expected)}'`)
+      printLastResult()
+      throw err
+    }
+  }
+
+  function assert (actual) {
+    try {
+      _assert(actual)
+    } catch (err) {
+      console.log('assert failed')
+      console.log(`actual='${json(actual)}'`)
+      printLastResult()
+      throw err
+    }
+  }
 
   async function get (expect, path, options = {}) {
     printExpect(expect)
     const url = `${BASE_URL}${path}`
     console.log(`http get '${url}' ${headerString(options.headers)}`)
     const result = await axios.get(url, options)
-    requests.push({method: 'GET', path, options, result})
+    self.requests.push({method: 'GET', path, options, result, suite: self.suite})
     assertEqual(result.status, expect.status || 200)
     return result
   }
@@ -52,7 +76,7 @@ function client ({BASE_URL}) {
     const url = `${BASE_URL}${path}`
     console.log(`http post '${url}' ${dataString(data)} ${headerString(options.headers)}`)
     const result = await axios.post(url, data, options)
-    requests.push({method: 'POST', path, data, options, result})
+    self.requests.push({method: 'POST', path, data, options, result, suite: self.suite})
     assertEqual(result.status, expect.status || 200)
     return result
   }
@@ -62,7 +86,7 @@ function client ({BASE_URL}) {
     const url = `${BASE_URL}${path}`
     console.log(`http put '${url}' ${dataString(data)} ${headerString(options.headers)}`)
     const result = await axios.put(url, data, options)
-    requests.push({method: 'PUT', path, data, options, result})
+    self.requests.push({method: 'PUT', path, data, options, result, suite: self.suite})
     assertEqual(result.status, expect.status || 200)
     return result
   }
@@ -72,12 +96,12 @@ function client ({BASE_URL}) {
     const url = `${BASE_URL}${path}`
     console.log(`http delete '${url}' ${headerString(options.headers)}`)
     const result = await axios.delete(url, options)
-    requests.push({method: 'DELETE', path, options, result})
+    self.requests.push({method: 'DELETE', path, options, result, suite: self.suite})
     assertEqual(result.status, expect.status || 200)
     return result
   }
 
-  return {
+  Object.assign(self, {
     assert,
     assertEqual,
     isMongoId,
@@ -86,7 +110,8 @@ function client ({BASE_URL}) {
     post,
     put,
     delete: _delete
-  }
+  })
+  return self
 }
 
 module.exports = client
